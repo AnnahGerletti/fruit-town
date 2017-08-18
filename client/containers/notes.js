1c505459b483933af0e79dcc3841fe7ff49c805c
eda013 <poukupenga@gmail.com>
2017-08-18 15:49:45 +1200

    added some styling and made notes in container

---
+++ client/containers/notes.js
@@ -0,0 +1,6 @@
+//the container is where you put all the connected parts of the compt.
+//the compt file and container file are both .jsx
+//just the redux stuff, actions and dispatch comands.
+//Having a container makes testing componets easier
+//it removes the complex parts.
+//things to do when testing Compt, be specific about props, disconnect compt and be specific about how you want it to behave. 
